6ada833cdb97ad69171dc841143690d46c1c799c
anon.imous <anonymous@nowhere.com>
2018-01-07 03:24:37 +0100

    updated other files

---
+++ gen/com/pikamander2/japanesequiz/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.pikamander2.japanesequiz;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
